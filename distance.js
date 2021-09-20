77b6e460cfa67282324fc969d6c3826e8659b536
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-09-20 15:04:57 -0400

    Update distance.js

---
+++ distance.js
@@ -0,0 +1,21 @@
+function Point(x1,y1,x2,y2){
+// let x1 = 8;
+// let y1 = 2;
+// let x2 = 10;
+// let y2 = 3;
+   
+        return Math.sqrt((Math.pow(x1-x2,2))+(Math.pow(y1-y2,2)))
+
+}
+
+var points = Point (8,2,10,3);
+console.log(points)
+
+
+// const x1 = 8;
+// const y1 = 3;
+// const x2 = 5;
+// const y2 = 2;
+
+// const distance = x1 - x2  + y2 - y1 
+// console.log('distance')
